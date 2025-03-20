8f72064682344ca0afc4524f771739e584433bfe
Tausif Alam <tausifalam87654321@gmail.com>
2025-03-20 20:45:55 +0530

    second commit

--- server/utils/verifyEmailTemplate.js
+++ server/utils/verifyEmailTemplate.js
@@ -1,7 +1,7 @@
 const verifyEmailTemplate = ({name, url}) => {
     return `
     <p>Dear ${name} </p>
-    <p>Thank you for registering to Blinkit</p>
+    <p>Thank you for registering to Blinkit web.</p>
     <a href=${url} style= "color : white; background : blue: margin-top : 10px ">
      Verify Email
     </a> 
