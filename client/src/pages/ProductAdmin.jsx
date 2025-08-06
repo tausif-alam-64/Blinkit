6461fe65e1269512f9db830ebdb1eca45a803276
Tausif Alam <tausifalam87654321@gmail.com>
2025-08-06 23:25:12 +0530

    search machanism

--- client/src/pages/ProductAdmin.jsx
+++ client/src/pages/ProductAdmin.jsx
@@ -10,6 +10,7 @@ const ProductAdmin = () => {
   const [productData, setProductData] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [totalPageCount, setTotalPageCount] = useState(1);
 
   const fetchProductData = async () => {
     try {
@@ -18,7 +19,7 @@ const ProductAdmin = () => {
         ...SummaryApi.getProduct,
         data: {
           page: page,
-          limit: 12
+          limit: 12,
         },
       });
 
@@ -36,7 +37,18 @@ const ProductAdmin = () => {
 
   useEffect(() => {
     fetchProductData();
-  }, []);
+  }, [page]);
+
+  const handleNext = () => {
+    if (page !== totalPageCount) {
+      setPage((prev) => prev + 1);
+    }
+  };
+  const handlePrevious = () => {
+    if (page > 1) {
+      setPage((prev) => prev - 1);
+    }
+  };
   return (
     <section>
       <div className="p-2 bg-white shadow-md flex items-center justify-between">
@@ -45,10 +57,30 @@ const ProductAdmin = () => {
       {loading && <Loading />}
 
       <div className="p-4 bg-blue-50">
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
-          {productData.map((p, index) => {
-            return <ProductCardAdmin data={p} />;
-          })}
+        <div className="min-h-[55vh]">
+          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+            {productData.map((p, index) => {
+              return <ProductCardAdmin data={p} />;
+            })}
+          </div>
+        </div>
+
+        <div className="flex justify-between my-4 ">
+          <button
+            onClick={handlePrevious}
+            className="border rounded border-primary-200 px-4 py-1 hover:bg-primary-200"
+          >
+            Previous
+          </button>
+          <button className="w-full bg-slate-50">
+            {page}/{totalPageCount}{" "}
+          </button>
+          <button
+            onClick={handleNext}
+            className="border rounded border-primary-200 px-4 py-1 hover:bg-primary-200"
+          >
+            Next
+          </button>
         </div>
       </div>
     </section>
