4e470b6c4b514e35567283454dec6b369b6a5b14
Tausif Alam <tausifalam87654321@gmail.com>
2025-08-09 19:15:28 +0530

    start working on homePage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 const Home = () => {
   return (
-    <div>
-      home
-    </div>
+    <section>
+      <div className="container mx-auto my-4 px-4">
+        <div className="w-full h-full min-h-48 rounded bg-blue-100 animate-pulse">
+
+        </div>
+      </div>
+    </section>
   )
 }
 
