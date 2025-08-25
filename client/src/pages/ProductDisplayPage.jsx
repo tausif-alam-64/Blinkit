e7fe16ca6e0192832a9d0587d8d3e0c4bf5f657d
Tausif Alam <tausifalam87654321@gmail.com>
2025-08-25 20:12:09 +0530

    working on product display page

--- client/src/pages/ProductDisplayPage.jsx
+++ client/src/pages/ProductDisplayPage.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const ProductDisplayPage = () => {
+  const params = useParams()
+  console.log(params)
   return (
-    <div>
-      ProductDisplayPage
-    </div>
+    <section className='sticky top-24 lg:top-20'>
+      <div className='container sticky top-24 mx-auto grid grid-cols-[90px,1fr] md:grid-cols-[200px,1fr]'>
+        {/* sub category */}
+        <div className=' min-h-[79vh]'>
+          
+        </div>
+
+       {/* product */}
+        <div className=''>
+          Product
+        </div>
+      </div>
+    </section>
   )
 }
 
