4965b55033a391ebf20357965b0830518ba7ce60
Tausif Alam <tausifalam87654321@gmail.com>
2025-09-08 23:58:54 +0530

    making responsive to the productList page

--- client/src/pages/ProductListPage.jsx
+++ client/src/pages/ProductListPage.jsx
@@ -7,19 +7,19 @@ import AxiosToastError from "../utils/AxiosToastError";
 import { useEffect } from "react";
 import Loading from "../components/Loading";
 import CardProduct from "../components/CardProduct";
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 
 const ProductListPage = () => {
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(true);
   const [totalPage, setTotalPage] = useState(1);
-  const [displaySubCategory, setDisplaySubCategory] = useState([])
+  const [displaySubCategory, setDisplaySubCategory] = useState([]);
 
   const params = useParams();
 
-  const allSubCategory = useSelector(state => state.product.allSubCategory)
-   console.log(allSubCategory)
+  const allSubCategory = useSelector((state) => state.product.allSubCategory);
+  console.log(allSubCategory);
 
   const categoryId = params.category.split("-").slice(-1)[0];
   const subCategoryId = params.subCategory.split("-").slice(-1)[0];
@@ -62,34 +62,40 @@ const ProductListPage = () => {
     fetchProductData();
   }, [params]);
   useEffect(() => {
-    const sub = allSubCategory.filter(s => {
-      const filterData = s.category.some(el => {
-        return el._id == categoryId
-      })
-      return filterData ? filterData : null
-    })
-    setDisplaySubCategory(sub)
-  }, [params, allSubCategory])
+    const sub = allSubCategory.filter((s) => {
+      const filterData = s.category.some((el) => {
+        return el._id == categoryId;
+      });
+      return filterData ? filterData : null;
+    });
+    setDisplaySubCategory(sub);
+  }, [params, allSubCategory]);
 
   return (
     <section className="sticky top-24 lg:top-20">
-      <div className="container sticky top-24 mx-auto grid grid-cols-[100px,1fr] md:grid-cols-[200px,1fr]">
+      <div className="container sticky top-24 mx-auto grid grid-cols-[100px,1fr] md:grid-cols-[200px,1fr] lg:grid-cols-[280px,1fr]">
         {/* sub category */}
-        <div className=" min-h-[79vh] py-2 max-h-[79vh] overflow-scroll grid gap-2 shadow-md scrollBarCustom">
-          {
-            displaySubCategory.map((s,index) => {
-              return (
-                <div className="w-full p-2 bg-white">
-                  <div className="w-fit mx-auto">
-                    <img src={s.image} alt={s.name} className="w-20 h-full object-scale-down" />
-                  </div>
-                  <p className="-mt-5 text-xs text-center lg:text-base">
-                    {s.name}
-                  </p>
+        <div className=" min-h-[79vh] py-2 bg-white max-h-[79vh] overflow-y-scroll grid gap-1 shadow-md scrollBarCustom">
+          {displaySubCategory.map((s, index) => {
+            return (
+              <div
+                className={`w-full p-2 lg:flex items-center lg:w-full lg:h-16 box-border lg:gap-4 border-b hover:bg-green-100 cursor-pointer ${
+                  subCategoryId === s._id ? "bg-green-100" : ""
+                }`}
+              >
+                <div className="w-fit max-w-28 lg:mx-0 rounded box-border mx-auto bg-white">
+                  <img
+                    src={s.image}
+                    alt={s.name}
+                    className="w-14 lg:h-14 lg:w-12 h-full "
+                  />
                 </div>
-              )
-            })
-          }
+                <p className="-mt-5 text-xs text-center lg:text-base">
+                  {s.name}
+                </p>
+              </div>
+            );
+          })}
         </div>
 
         {/* product */}
@@ -99,18 +105,14 @@ const ProductListPage = () => {
           </div>
           <div>
             <div className="grid grid-cols-1 p-4 gap-4 md:grid-cols-3 lg:grid-cols-5">
-              {
-                data.map((p, index) => {
-                  return(
-                    <CardProduct 
-                     data={p}
-                     key={p._id+"productsub"+index}
-                     />
-                  )
-                })
-              }
+              {data.map((p, index) => {
+                return (
+                  <CardProduct data={p} key={p._id + "productsub" + index} />
+                );
+              })}
             </div>
-            {loading && <Loading />}</div>
+            {loading && <Loading />}
+          </div>
         </div>
       </div>
     </section>
