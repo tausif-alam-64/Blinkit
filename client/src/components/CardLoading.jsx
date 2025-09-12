e0d07ea1d6a9f82f826d564dacbf5e3f90dec463
Tausif Alam <tausifalam87654321@gmail.com>
2025-09-12 23:58:49 +0530

    working on Search page

--- client/src/components/CardLoading.jsx
+++ client/src/components/CardLoading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CardLoading = () => {
   return (
-    <div className='border p-4 grid gap-3 max-w-36 lg:max-w-52 rounded animate-pulse'>
+    <div className='border p-4 grid gap-3 min-w-36 lg:max-w-52 rounded animate-pulse'>
       <div className='min-h-14 lg:min-h-20 bg-blue-50 rounded'>  
       </div>
       <div className='p-3 max-sm:p-2 bg-blue-50 rounded w-20'>
--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CardLoading from '../components/CardLoading'
 
 const SearchPage = () => {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const loadingArrayCard = new Array(10).fill(null)
   return (
-    <div>
-      SearchPage
-    </div>
+    <section className='bg-white'>
+      <div className='mx-auto container p-4'>
+        <p className='font-semibold'>Search Results : {data.length}</p>
+
+        <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5'>
+          {
+            loading && (
+              loadingArrayCard.map((_,index) => {
+                return (
+                  <CardLoading key={"loading" + index} />
+                )
+              })
+            )
+          }
+        </div>
+      </div>
+    </section>
   )
 }
 
